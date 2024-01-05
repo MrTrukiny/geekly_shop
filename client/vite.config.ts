71f7da637732f0eeb168dab58ebdbd311bb0004c
MrTrukiny <carloseduardo.tkn@gmail.com>
2024-01-04 19:43:04 -0500

    fix: properly add proxy to allow browsers to send the jwt cookie as header

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      // Using the key as the matching path and the value as the proxy target
+      '/api': 'http://localhost:5000', // This proxies any requests to /api to http://localhost:3000
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
